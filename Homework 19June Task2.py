59acf2766d5162bc1ebff1b32e6d228edc6b4874
Mannstrom <85886046+Mannstrom@users.noreply.github.com>
2021-06-21 15:32:39 +0200

    Add files via upload

---
+++ Homework 19June Task2.py
@@ -0,0 +1,5 @@
+phone_number = "0701234567"
+if len(phone_number) == 10 and phone_number.isdigit():
+    print ("This is a valid 10 digit phonenumber.")
+else:   
+    print ("Invalid Phonenumber")    
---
+++ Homework 19June task1.py
@@ -0,0 +1,3 @@
+my_name = "mattias"
+print ("Characters in my name:",len(my_name))
+print (my_name[0:2] + my_name[-2:])
